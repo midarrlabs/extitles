055778b19bbfe0ef2db1b4c7d786e14094610a71
trueChazza <cgnetwork.nz@gmail.com>
2022-07-15 15:38:37 +1200

    build: initial resources

---
+++ lib/extitles.ex
@@ -0,0 +1,18 @@
+defmodule Extitles do
+  @moduledoc """
+  Documentation for `Extitles`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Extitles.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/extitles_test.exs
@@ -0,0 +1,8 @@
+defmodule ExtitlesTest do
+  use ExUnit.Case
+  doctest Extitles
+
+  test "greets the world" do
+    assert Extitles.hello() == :world
+  end
+end
