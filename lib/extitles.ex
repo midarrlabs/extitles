e47466fbacc3957498cea0fbe786c37dc5ae6584
trueChazza <cgnetwork.nz@gmail.com>
2022-07-17 11:11:43 +1200

    docs: add doctests

--- lib/extitles.ex
+++ lib/extitles.ex
@@ -15,16 +15,6 @@ defmodule Extitles do
     ]
   end
 
-  def parse(path) do
-    File.read!(path)
-    |> String.split("\n\n", trim: true)
-    |> Enum.flat_map(fn item ->
-      item
-      |> String.split("\n", trim: true)
-      |> make_cue
-    end)
-  end
-
   defp pad(time, count \\ 2), do: "#{time}" |> String.pad_leading(count, ["0"])
 
   defp format_time(
@@ -42,6 +32,34 @@ defmodule Extitles do
     |> Enum.join("\n\n")
   end
 
+  @doc ~S"""
+  Parses.
+
+  ## Examples
+
+      iex> Extitles.parse("test/fixtures/example.srt")
+      [%{from: ~T[00:05:00.400], to: ~T[00:05:15.300], text: ["This is an example of", "a subtitle."]}, %{from: ~T[00:05:16.400], to: ~T[00:05:25.300], text: ["This is an example of", "a subtitle - 2nd subtitle."]}]
+
+  """
+  def parse(path) do
+    File.read!(path)
+    |> String.split("\n\n", trim: true)
+    |> Enum.flat_map(fn item ->
+      item
+      |> String.split("\n", trim: true)
+      |> make_cue
+    end)
+  end
+
+  @doc ~S"""
+  Formats.
+
+  ## Examples
+
+      iex> Extitles.format(Extitles.parse("test/fixtures/example.srt"))
+      "WEBVTT\n\n00:05:00.400 --> 00:05:15.300\nThis is an example of\na subtitle.\n\n00:05:16.400 --> 00:05:25.300\nThis is an example of\na subtitle - 2nd subtitle."
+
+  """
   def format(cues) do
     "WEBVTT\n\n" <> build_cues(cues)
   end
